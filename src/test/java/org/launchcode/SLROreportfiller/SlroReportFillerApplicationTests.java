d723e9928e2bab771b73dd3eb82653e4535d1542
Amy Stockhausen <stockhausenamy@gmail.com>
2023-04-23 08:42:10 -0500

    Initial build with Spring Boot.

---
+++ src/test/java/org/launchcode/SLROreportfiller/SlroReportFillerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.SLROreportfiller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlroReportFillerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
